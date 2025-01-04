fdb899ff6478dd72e234b94fa423b594b158e271
Narendra Mahara <narendramahara9868@gmail.com>
2025-01-04 12:18:34 +0545

    add cookie-parser and cors middleware; update app and index files for integration

--- megaProject/src/index.js
+++ megaProject/src/index.js
@@ -1,14 +1,15 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({ path: "./.env" });
 
 connectDB()
   .then(() => {
-    console.log(
-      `Server is running at port no: ${process.env.PORT || 5000}`
-    );
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server is running at port no: ${process.env.PORT || 5000}`);
+    });
   })
   .catch((err) => {
-    console.log("Mongodb connection failed !!!",err);
+    console.log("Mongodb connection failed !!!", err);
   });
