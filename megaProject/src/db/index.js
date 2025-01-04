77e24ae11b183b15360892fccee399674d26580f
Narendra Mahara <narendramahara9868@gmail.com>
2025-01-04 12:03:49 +0545

    enhance database connection handling and server startup logging

--- megaProject/src/db/index.js
+++ megaProject/src/db/index.js
@@ -3,8 +3,6 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
-    console.log(process.env.MONGODB_URI);
-    
     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
     console.log(`Database connected successfully !!!`);
   } catch (error) {
--- megaProject/src/index.js
+++ megaProject/src/index.js
@@ -3,4 +3,12 @@ import connectDB from "./db/index.js";
 
 dotenv.config({ path: "./.env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    console.log(
+      `Server is running at port no: ${process.env.PORT || 5000}`
+    );
+  })
+  .catch((err) => {
+    console.log("Mongodb connection failed !!!",err);
+  });
