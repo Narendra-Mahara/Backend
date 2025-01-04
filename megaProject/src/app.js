1d16e56d99969009bed8565b337977b2798c2846
Narendra Mahara <narendramahara9868@gmail.com>
2025-01-04 12:35:17 +0545

    configure CORS and enhance middleware for JSON, URL-encoded data, and static file handling

--- megaProject/src/app.js
+++ megaProject/src/app.js
@@ -1,10 +1,18 @@
-import express  from "express";
+import express from "express";
 import cookieParser from "cookie-parser";
-import { cors } from "cors";
+import cors from "cors";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
-
-
+app.use(express.json({ limit: "16kb" })); // used to accept json data
+app.use(express.urlencoded({ limit: "16kb" })); // used to accept form data
+app.use(express.static("public")); // used to serve static files
+app.use(cookieParser()); // used to parse cookies
 export default app;
