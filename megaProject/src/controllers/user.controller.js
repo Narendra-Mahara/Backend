1ad73d466b179678630a0433154021c3130bb49d
Narendra Mahara <narendramahara9868@gmail.com>
2025-06-04 11:15:27 +0545

    add updateUserAvatar method

--- megaProject/src/controllers/user.controller.js
+++ megaProject/src/controllers/user.controller.js
@@ -272,6 +272,31 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
     .json(new apiResponse(200, user, "Account details updated successfully!"));
 });
 
+const updateUserAvatar = asyncHandler(async (req, res) => {
+  const avatarLocalPath = req.file?.path;
+  if (!avatarLocalPath) {
+    return new apiError(400, "Avatar file is required!");
+  }
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  if (!avatar.url) {
+    return new apiError(400, "Error while uploading avatar!");
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        avatar: avatar.url,
+      },
+    },
+    { new: true }
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(new apiResponse(200, user, "Avatar updated successfully!"));
+});
+
 export {
   registerUser,
   loginUser,
